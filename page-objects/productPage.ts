20db16571421169e393cd93bc622ba7e40308ff8
Aleksa Kljucar <aleksa.kljucar@gmail.com>
2024-04-24 14:59:38 +0200

    Created functions for review functionality

--- page-objects/productPage.ts
+++ page-objects/productPage.ts
@@ -1,7 +1,6 @@
 import { Page } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 import fs from 'fs'
-
 export class ProductPage{
 
     private readonly page:Page
@@ -10,32 +9,83 @@ export class ProductPage{
         this.page = page
     }
 
+    /**
+     * Function that hovers product that we type in parameters hover over it and click on review
+     * @param productTitle - product title
+     * @param sectionTitle - section title
+     */
     async selectProductFromSection(productTitle: any, sectionTitle: string){
+        //read sections.json file where are ids for all sections
         let sectionsJSON = fs.readFileSync('/Users/ivicamacbook/Desktop/ALEKSA/AutomationTestStore/AutomationTestStoreInternship/data/sections.json', 'utf-8')
+        //parse it into sections variable
         let sections = JSON.parse(sectionsJSON)
+        //loop thourgh sections
         for(let section in sections){
+            //if section in sections.json matches sectionTitle provided in function parameter
             if(section === sectionTitle){
-                const special = await this.page.locator(`${sections[section]}`).scrollIntoViewIfNeeded()
+                //scroll to the section
+                await this.page.locator(`${sections[section]}`).scrollIntoViewIfNeeded()
+                //call writeData function to get div text
                 let element = await this.writeData(sectionTitle, productTitle)
+                //hover over certain product 
                 await this.page.locator(`${sections[section]} > .thumbnails > ${element} > .thumbnail > a`).hover()
+                //click on write review link
                 await this.page.getByRole('link', { name: 'Write Review' }).click()
             }
         }
     }
-
+    /**
+     * Function that returns div:nth-child value for product in a section
+     * @param sectionTitle - section title same as in selectProductFromSection function
+     * @param productTitle - pruduct title same as in selectProductFromSection function
+     * @returns - divText string with exact div:nth-child value for certain product
+     */
     async writeData(sectionTitle:string, productTitle: string){
+        //create string array that consists out of products in certain section
         let productArray = (await this.page.locator(`#${sectionTitle} .prdocutname`).allTextContents())
+        //find element that matches productTitle 
         const elements = (element:string) => element === productTitle
+        //find what is the index of that element in array
         const nthValue = productArray.findIndex(elements)
+        //create string where nth-value if index of that element + 1
         const divText = `div:nth-child(${nthValue + 1})`
+        //return that string
         return divText
     }
-
+    /**
+     * Function that fills review form
+     * @param stars 
+     */
     async fillWriteReview(stars: string){
-        //await this.page.locator(`.star-rating rater-0 star star-rating-applied star-rating-live #rating${stars}`).click()
+        //Select star based on number of stars provided in paramethers
         await this.page.locator('a').getByText(stars).first().click()
-        await this.page.locator('#name').fill(faker.person.fullName())
-        await this.page.locator('#text').fill(faker.lorem.words(50))
+        //fill name input with random fullName from faker.js
+        const fullName = faker.person.fullName()
+        await this.page.locator('#name').fill(fullName)
+        //fill review input with random lorem text from faker.js
+        const reviewText = faker.lorem.words(50)
+        await this.page.locator('#text').fill(reviewText)
+        //click on the submit button
         await this.page.getByRole('button', {name: " Submit"}).click()
+        return {
+            first: fullName,
+            second: reviewText
+        }
+    }
+
+    async getStars(stars: string){
+        return this.page.locator('a').getByText(stars)
+    }
+
+    async getName(){
+        return this.page.locator('#name').textContent()
+    }
+
+    async getReview(){
+        return this.page.locator('#text')
+    }
+
+    async getAlertHumanVerification(){
+        return this.page.locator('.alert alert-error alert-danger')
     }
 }
--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,13 +1,5 @@
 import { Page, expect, test } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
-import puppeteer from 'puppeteer-extra'
-import StealthPlugin from 'puppeteer-extra-plugin-stealth'
-
-
-puppeteer
-  .use(StealthPlugin())
-  .launch({ headless: true })
-  .then(async browser => {})
 
 test.describe('review', () => {
     let pm:any
@@ -20,7 +12,12 @@ test.describe('review', () => {
 
     test('add review to a product in special section', async({page}) =>{
        await pm.productsPage().selectProductFromSection("Absolute Anti-Age Spot Replenishing Unifying TreatmentSPF 15","latest")
-       //await page.waitForTimeout(1000)
-       await pm.productsPage().fillWriteReview('3')
+       const value = await pm.productsPage().fillWriteReview('3')
+       const nameText = await pm.productsPage().getName()
+       const reviewText = await pm.productsPage().getReview()
+       const alertText = await pm.productsPage().getAlertHumanVerification()
+       await expect(nameText).toHaveText(value.first)
+       await expect(reviewText).toContainText(value.second)
+       await expect(alertText).toHaveText(alertText)
     })
 })
