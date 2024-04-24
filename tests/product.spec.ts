df12f969822818ee8ce5de63dd4566f8b7df8058
Aleksa Kljucar <aleksa.kljucar@gmail.com>
2024-04-24 16:00:04 +0200

    deleted Page from import

--- tests/product.spec.ts
+++ tests/product.spec.ts
@@ -1,4 +1,4 @@
-import { Page, expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
 
 test.describe('review', () => {
